3fc7182e17d7f90d191a17a393dcdb5eb7694fda
EWolf <e.wolf@yudole.ru>
2025-08-28 22:46:42 +0300

    Fix

--- hub.go
+++ hub.go
@@ -15,33 +15,69 @@ import (
 
 const RECONNECT_TIMEOUT = 5 * time.Second
 
+type Ability struct {
+	Service string
+	Value   string
+}
+
 var outChan = make(chan any, 9999)
 var InChan = make(chan msg.Event, 9999)
-var NetStateChan = make(chan string, 999)
+
+var Connected bool
+var Abilities []Ability // Массив задач которые решает клиент (абилки). Возможная проблема такого решения - будут приниматься и отзываться сразу все абилки, но, т.к. пока не планируется создавать в одном клиенте несколько независимых друг от друга набора абилок - такой вариант выглядит оптимальным на данный момент оптимальным
+var isAccepted bool     // Если флаг установлен - после подключения/реконнекта отправляем список абилок на сервер
+
+// Отправляем на сервер список абилок клиента
+func Accept() {
+	isAccepted = true
+
+	for _, ability := range Abilities {
+		outChan <- msg.EventPayload{
+			Event: msg.Event{
+				Type:    "ability.accept",
+				Value:   ability.Value,
+				Service: ability.Service,
+			},
+		}
+	}
+}
+
+// Отправляем на сервер список абилок клиента (отключаем)
+func Drop() {
+	isAccepted = false
+
+	for _, ability := range Abilities {
+		outChan <- msg.EventPayload{
+			Event: msg.Event{
+				Type:    "ability.drop",
+				Value:   ability.Value,
+				Service: ability.Service,
+			},
+		}
+	}
+}
 
 func Connect(host, port string) {
-	ctx, cancel := context.WithCancel(context.Background()) // Контекст для отмены отправки данных в сокет Хаба
-	defer reconnect(cancel, host, port)
+	defer reconnect(host, port)
 
 	var conn, err = net.Dial("tcp", fmt.Sprintf("%s:%s", host, port))
-
-	defer func() {
-		if conn != nil {
-			conn.Close()
-		}
-	}()
-
 	if err != nil {
 		log.Printf("Could not connect to HUB: %s\n", err)
 		return
 	}
 
-	NetStateChan <- "connected"
+	ctx, stop := context.WithCancel(context.Background()) // Контекст для отмены отправки данных в сокет Хаба
+	defer conn.Close()                                    // Закрываем Сокет
+	defer stop()                                          // Останавливаем Reader и Writer
 
-	fmt.Println("HUB connection established")
+	if isAccepted {
+		Accept()
+	}
+
+	fmt.Printf("HUB connection established to: %s:%s\n", host, port)
 
 	go writer(ctx, conn)
-	reader(conn)
+	reader(ctx, conn)
 }
 
 func Send(message any) {
@@ -64,20 +100,10 @@ func JoinFailed(service, channel string) {
 	}
 }
 
-func reconnect(cancel context.CancelFunc, host, port string) {
-	cancel()
-
-	NetStateChan <- "reconnecting"
+func reconnect(host, port string) {
+	outChan = make(chan any, 9999)
 
 	log.Println("HUB connection lost. Reconnecting...")
-
-	for len(InChan) > 0 {
-		<-InChan
-	}
-
-	for len(outChan) > 0 {
-		<-outChan
-	}
 
 	time.Sleep(RECONNECT_TIMEOUT)
 	Connect(host, port)
@@ -87,24 +113,32 @@ func Event(event msg.EventPayload) {
 	outChan <- event
 }
 
-func reader(conn net.Conn) {
+func reader(ctx context.Context, conn net.Conn) {
 	var dec = jsontext.NewDecoder(conn)
 
 	for {
-		var message msg.Event
-		err := json.UnmarshalDecode(dec, &message)
+		select {
+		case <-ctx.Done(): // Завершаем Читателя
+			log.Println("HUB Reader stopped")
+			return
 
-		if err == io.EOF {
-			conn.Close()
-			return
-		} else if err != nil {
-			conn.Close()
-			fmt.Println(err)
-			return
+		default:
+			var message msg.Event
+			err := json.UnmarshalDecode(dec, &message)
+
+			if err == io.EOF {
+				conn.Close()
+				return
+			} else if err != nil {
+				conn.Close()
+				fmt.Println(err)
+				return
+			}
+
+			fmt.Println("HUB incoming message:", message)
+			InChan <- message
 		}
 
-		fmt.Println("HUB_IN_CHAN:", message)
-		InChan <- message
 	}
 }
 
@@ -116,13 +150,16 @@ func writer(ctx context.Context, conn net.Conn) {
 		case message := <-outChan:
 			err := json.MarshalEncode(enc, message)
 
-			if err != nil {
+			if err != nil { // Ошибка отправки сообщения в Сокет
+				log.Println(err)
 				conn.Close()
-				log.Println(err) // Возможно вывод этой ошибки не требуется
 				return
 			}
 
-		case <-ctx.Done():
+			log.Println("HUB out message:", message)
+
+		case <-ctx.Done(): // Завершаем Писателя
+			log.Println("HUB Writer stopped:", ctx.Err())
 			return
 		}
 
